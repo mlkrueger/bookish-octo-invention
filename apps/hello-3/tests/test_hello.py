ebf04189f6a1a867183b5156d58446e2d419848f
Matthew Krueger <matthew@peakventures.co>
2024-07-02 08:25:42 -0500

    adding hello-3

---
+++ apps/hello-3/tests/test_hello.py
@@ -0,0 +1,13 @@
+import pytest
+from bookish_octo_invention import hello
+
+@pytest.mark.parametrize(
+    'name', ['Matthew','Octo-Cat','World','', None]
+)
+def test_hello(name:str) -> None:
+    if name not in [None, '']:
+        greeting = hello.hello(name)
+        assert greeting == f"Hello, {name}!"
+    else:
+        with pytest.raises(IOError):
+            greeting = hello.hello(name)
